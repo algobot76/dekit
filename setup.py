2646e81391f8a7ab4256595a78a2ddf34e13cd7b
Kaitian Xie <xkaitian@gmail.com>
2019-10-15 21:56:49 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,4 @@
-# -*- coding: utf-8 -*-
-
-# Learn more: https://github.com/kennethreitz/setup.py
-
 from setuptools import setup, find_packages
-
 
 with open('README.rst') as f:
     readme = f.read()
@@ -12,14 +7,13 @@ with open('LICENSE') as f:
     license = f.read()
 
 setup(
-    name='sample',
-    version='0.1.0',
-    description='Sample package for Python-Guide.org',
+    name='dekit',
+    version='0.0.1',
+    description='A collection of useful Python decorators',
     long_description=readme,
-    author='Kenneth Reitz',
-    author_email='me@kennethreitz.com',
-    url='https://github.com/kennethreitz/samplemod',
+    author='Kaitian Xie',
+    author_email='xkaitian@gmail.com',
+    url='https://github.com/algobot76/dekit',
     license=license,
     packages=find_packages(exclude=('tests', 'docs'))
 )
-
