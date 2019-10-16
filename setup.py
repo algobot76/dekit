f92fc7bf5dc7038f876fa4fb341f6de3962d3d3d
Kaitian Xie <xkaitian@gmail.com>
2019-10-16 01:54:02 -0700

    Update README and setup.py before package release

--- setup.py
+++ setup.py
@@ -1,19 +1,17 @@
 from setuptools import setup, find_packages
 
-with open('README.rst') as f:
+with open('README.rst', 'r') as f:
     readme = f.read()
-
-with open('LICENSE') as f:
-    license = f.read()
 
 setup(
     name='dekit',
     version='0.0.1',
     description='A collection of useful Python decorators',
     long_description=readme,
+    long_description_content_type="text/x-rst",
     author='Kaitian Xie',
     author_email='xkaitian@gmail.com',
     url='https://github.com/algobot76/dekit',
-    license=license,
+    license='MIT',
     packages=find_packages(exclude=('tests', 'docs'))
 )
